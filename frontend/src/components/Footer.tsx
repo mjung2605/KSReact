9cc78cd7dcd3c5f1a528e2039359b83e99e497fd
Meike <92315934+mjung2605@users.noreply.github.com>
2024-05-12 00:46:51 +0200

    fixed footer/scrolling

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,5 +1,5 @@
 export const Footer = () => {
-    return <footer className="z-100 relative -top-20 h-20 px-40 flex justify-evenly items-center bg-ksPink">
+    return <footer className="z-100 h-20 px-40 flex justify-evenly items-center bg-ksPink">
     <p className="text-xl">&copy; 2024</p>
     <p className="text-xl">All Rights Reserved</p>
     <p className="text-xl"><a href="impressum.html" className="text-ksBlack visited:text-ksBlack hover:text-ksBlack hover:underline active:text-ksBlack active:underline">Impressum</a></p>
--- frontend/src/pages/home/Contact.tsx
+++ frontend/src/pages/home/Contact.tsx
@@ -2,7 +2,8 @@ import { BlurryBackground, ContactForm } from "@/components"
 
 export const Contact = () => {
 
-    return <section style={{scrollSnapAlign: "start"}} className="overflow-x-hidden flex bg-contact-image bg-cover">
+    return <section style={{scrollSnapAlign: "start"}} className="overflow-hidden flex flex-col bg-contact-image bg-cover">
         <BlurryBackground title="Kontakt" content={<ContactForm />}/>
+        
     </section>
 }
