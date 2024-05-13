715c7f0a07457525d9384211006bdc4360824400
Meike <92315934+mjung2605@users.noreply.github.com>
2024-05-13 17:15:21 +0200

    styled error page

--- frontend/src/pages/errorpage.tsx
+++ frontend/src/pages/errorpage.tsx
@@ -2,8 +2,8 @@ import { Heading } from "@/components"
 
 export const ErrorPage = () => {
 
-    return <div className="text-center m-0 bg-ksPink">
-        <Heading title={"Fehler"} isTextWhite={false} isPageTitle={false} />
+    return <div className=" flex flex-col items-center  m-0 bg-ksPink h-screen w-screen">
+        <Heading title={"Ups!"} isTextWhite={false} isPageTitle={false} />
         <p>Da ist wohl leider etwas schiefgelaufen.</p>
         <p>Lade die Seite neu oder versuche es später erneut.</p>
     
