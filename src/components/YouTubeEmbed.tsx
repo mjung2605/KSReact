18ddfacb3dc5b9f0cec4cdda078ff097293c6e77
Meike Jungilligens <mjung2605@users.noreply.github.com>
2025-02-28 12:40:02 +0100

    deleted old video embed

--- src/components/YouTubeEmbed.tsx
+++
@@ -1,8 +0,0 @@
-
-export const YoutubeEmbed = () => {
-
-    return <div className="my-10 p-2 flex w-full drop-shadow-md justify-center">
-        <iframe className="aspect-video w-11/12 max-w-160 border-ksWhite border-8" src="https://www.youtube.com/embed/ZrXD5p31VDs?si=kDZo-1InkHoTWePS" title="Eingebettetes YouTube-Video"></iframe>
-    </div>
-
-}
